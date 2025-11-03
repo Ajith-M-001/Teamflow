a5ae947e88d596e8f18bcf1ba00edd3a6e21ec44
Ajith-M-001 <ajithguru741@gmail.com>
2025-11-03 19:25:32 +0530

    Enhance MessageList component with infinite scroll and new message notification

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/MessageList.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/MessageList.tsx
@@ -3,17 +3,25 @@
 import { orpc } from "@/lib/orpc";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { MessageItem } from "./message/MessageItem";
+import { nullable } from "zod";
+import { Button } from "@/components/ui/button";
 
 export function MessageList() {
   const { channelId } = useParams<{ channelId: string }>();
+  const [hasInitialScrolled, setHasInitialScrolled] = useState<boolean>(false);
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+  const lastItemRef = useRef<string | undefined>(undefined);
+  const [isAtBottom, setIsAtBottom] = useState<boolean>(false);
+  const [newMessages, setNewMessages] = useState<boolean>(false);
 
   const infiniteOptions = orpc.message.list.infiniteOptions({
     input: (pageParams: string | undefined) => ({
       channelId: channelId,
       cursor: pageParams,
-      limit: 30,
+      limit: 10,
     }),
     initialPageParam: undefined,
     getNextPageParam: (lastPage) => lastPage.nextCursor,
@@ -35,23 +43,98 @@ export function MessageList() {
     isFetchingNextPage,
     isLoading,
     error,
+    isFetching,
   } = useInfiniteQuery({
     ...infiniteOptions,
     staleTime: 30_000,
     refetchOnWindowFocus: false,
   });
 
+  useEffect(() => {
+    if (!hasInitialScrolled && data?.pages.length) {
+      const el = scrollRef.current;
+      if (el) {
+        el.scrollTop = el.scrollHeight;
+        setHasInitialScrolled(true);
+        setIsAtBottom(true);
+      }
+    }
+  }, [hasInitialScrolled, data?.pages.length]);
+
   const items = useMemo(() => {
     return data?.pages.flatMap((page) => page.items) ?? [];
   }, [data]);
 
+  const isNearBottom = (el: HTMLDivElement) => {
+    return el.scrollHeight - el.scrollTop - el.clientHeight <= 80;
+  };
+
+  const handleScroll = () => {
+    const el = scrollRef.current;
+    if (!el) return;
+    if (el.scrollTop <= 80 && hasNextPage && !isFetching) {
+      const prevScrollHeight = el.scrollHeight;
+      const prevScrollTop = el.scrollTop;
+      fetchNextPage().then(() => {
+        const newScrollHeight = el.scrollHeight;
+        el.scrollTop = newScrollHeight - prevScrollHeight + prevScrollTop;
+      });
+    }
+
+    setIsAtBottom(isNearBottom(el));
+  };
+
+  useEffect(() => {
+    if (!items?.length) return;
+
+    const lastId = items[items.length - 1].id;
+
+    const prevLastId = lastItemRef.current;
+    const el = scrollRef.current;
+
+    if (prevLastId && lastId !== prevLastId) {
+      if (el && isNearBottom(el)) {
+        requestAnimationFrame(() => {
+          el.scrollTop = el.scrollHeight;
+        });
+
+        setNewMessages(false);
+        setIsAtBottom(true);
+      } else {
+        setNewMessages(true);
+      }
+    }
+
+    lastItemRef.current = lastId;
+  }, [items]);
+
+
+  const scrollToBottom = () => {
+    const el = scrollRef.current;
+    if (!el) return;
+
+    el.scrollTop = el.scrollHeight;
+    setNewMessages(false);
+    setIsAtBottom(true);
+  }
+
   return (
     <div className="relative h-full">
-      <div className="h-full overflow-y-auto px-4">
+      <div
+        className="h-full overflow-y-auto px-4"
+        ref={scrollRef}
+        onScroll={handleScroll}
+      >
         {items?.map((msg) => (
           <MessageItem key={msg.id} message={msg} />
         ))}
+
+        <div ref={bottomRef}></div>
       </div>
+
+      {newMessages && !isAtBottom ? (
+        <Button onClick={scrollToBottom} type="button" className="absolute bottom-4 right-8 rounded-full ">New Messages</Button>
+      ) : null}
     </div>
   );
 }
